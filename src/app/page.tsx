c7f6c6cb912d2b80266fcbd31651e6af05c3e04f
farbubby <farhan.mahbub@ucf.edu>
2025-01-05 14:39:11 -0500

    Main page progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,57 @@
+"use client";
+
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
 export default function Home() {
+  const [submitted, setSubmitted] = useState(false);
+  const [itemCount, setItemCount] = useState(0);
+
+  const formList = Array.from({ length: itemCount }, (_, i) => (
+    <div key={i} className="flex flex-row gap-10 items-center">
+      <div className="font-bold">{i + 1}.</div>
+      <div className="w-full flex flex-row gap-5">
+        <div className="flex flex-col gap-2">
+          <Label>Item</Label>
+          <Input type="text" placeholder="Name" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label>Amount</Label>
+          <Input type="number" placeholder="10" />
+        </div>
+      </div>
+    </div>
+  ));
+
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <div className="flex flex-col items-center gap-4">
         <div className="text-2xl font-bold">
           How many items do you want to bring?
         </div>
-        <form className="flex flex-row gap-4 w-full justify-center items-center">
+        <form
+          className="flex flex-row gap-4 w-full justify-center items-center"
+          onSubmit={(e) => {
+            e.preventDefault();
+            setSubmitted(true);
+          }}>
           <input
             type="number"
             name="itemCount"
             id="itemCount"
-            required
             className="rounded-lg p-2 border border-gray-300 drop-shadow-lg w-1/2"
+            onChange={(e) => setItemCount(parseInt(e.target.value))}
           />
           <button
             type="submit"
@@ -31,6 +71,31 @@ export default function Home() {
             </svg>
           </button>
         </form>
+        {submitted && (
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline">List your items</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Items</DialogTitle>
+                <DialogDescription>
+                  Provide the name and quantity for each item you want to bring.
+                </DialogDescription>
+              </DialogHeader>
+              <form
+                className="flex flex-col gap-4"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                }}>
+                {formList}
+                <DialogFooter>
+                  <Button type="submit">Save changes</Button>
+                </DialogFooter>
+              </form>
+            </DialogContent>
+          </Dialog>
+        )}
       </div>
     </div>
   );
